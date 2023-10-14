7ddc23ea80cc8e8c869de020e25933a30a216c1f
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-14 04:55:33 +0000

    adding files for testing ruby

---
+++ howdy.rb
@@ -0,0 +1,10 @@
+my_string = "Howdy there!"
+
+pp my_string
+
+require "./goodbye.rb"
+
+require "active_support/all"
+
+pp 3.ordinalize
+
---
+++ name.rb
@@ -0,0 +1,9 @@
+pp "Hello! What's your name?"
+
+# user types
+
+user_name = gets
+
+pp "Hi there," + user_name + "!"
+
+#irb (command prompt to test)
